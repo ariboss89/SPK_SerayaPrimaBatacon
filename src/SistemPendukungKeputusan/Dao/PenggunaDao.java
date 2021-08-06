b742b26057c909693b5bb7a810727f4a0047b051
ariboss89 <ariboss89@gmail.com>
2021-08-07 00:37:16 +0700

    newupdate

---
+++ src/SistemPendukungKeputusan/Dao/PenggunaDao.java
@@ -0,0 +1,104 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Nova.Dao;
+
+import Nova.Koneksi.Koneksi;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author ariboss89
+ */
+public class PenggunaDao {
+    Koneksi con;
+    Statement st;
+    ResultSet res;
+    String query;
+    
+    public void Save(String username, String password) {
+        con = new Koneksi();
+        con.connect();
+        try {
+            st = con.conn.createStatement();
+            query = "insert into tb_admin(username, password, role)values('" + username + "','123456', 'ADMIN')";
+            st.executeUpdate(query);
+            st.close();
+            con.conn.close();
+            JOptionPane.showMessageDialog(null, "Data Berhasil di Simpan");
+        } catch (SQLException e) {
+        }
+    }
+    
+    public void Update(String username, String password) {
+        con = new Koneksi();
+        con.connect();
+        try {
+            st = con.conn.createStatement();
+            query = "update tb_admin set password ='"+password+"' where username = '" + username + "'";
+            st.executeUpdate(query);
+            st.close();
+            con.conn.close();
+            JOptionPane.showMessageDialog(null, "Data Berhasil di Update");
+        } catch (SQLException e) {
+
+        }
+    }
+    
+    public void Delete(String username) {
+        con = new Koneksi();
+        con.connect();
+        try {
+            st = con.conn.createStatement();
+            query = "delete from tb_admin where username = '" + username + "'";
+            st.executeUpdate(query);
+            st.close();
+            con.conn.close();
+            JOptionPane.showMessageDialog(null, "Data di Hapus");
+        } catch (SQLException e) {
+        }
+    }
+    
+    public String[][] Show() {
+
+        res = null;
+        String[][] data = null;
+        con = new Koneksi();
+        con.connect();
+        int jumlahBaris = 0;
+        try {
+            st = con.conn.createStatement();
+            query = "SELECT COUNT(username) AS Jumlah FROM tb_admin";
+            res = st.executeQuery(query);
+            if (res.next()) {
+                jumlahBaris = res.getInt("Jumlah");
+            }
+            query = "select *from tb_admin WHERE role = 'ADMIN'";
+            res = st.executeQuery(query);
+            data = new String[jumlahBaris][1];
+            int r = 0;
+            while (res.next()) {
+                data[r][0] = res.getString("username");
+                r++;
+            }
+            int jmlBaris = r;
+            String[][] tmpArray = data;
+            data = new String[jmlBaris][1];
+            for (r = 0; r < jmlBaris; r++) {
+                for (int c = 0; c <1; c++) {
+                    data[r][c] = tmpArray[r][c];
+                }
+            }
+            st.close();
+            con.conn.close();
+        } catch (SQLException e) {
+            System.err.println("SQLException : " + e.getMessage());
+        }
+        return data;
+    }
+}
---
+++ src/SistemPendukungKeputusan/Model/tb_admin.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Nova.Model;
+
+/**
+ *
+ * @author ariboss89
+ */
+public class tb_admin {
+    public static String role;
+    public static String keepUsername;
+    
+    
+    public String username;
+    public String password;
+
+    public static String getRole() {
+        return role;
+    }
+
+    public static void setRole(String role) {
+        tb_admin.role = role;
+    }
+
+    public static String getKeepUsername() {
+        return keepUsername;
+    }
+
+    public static void setKeepUsername(String keepUsername) {
+        tb_admin.keepUsername = keepUsername;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    
+    
+    
+    
+}
